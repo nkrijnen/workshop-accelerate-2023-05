2ec28368b341d00e91f7b98956ac7869272d1995
Nico Krijnen <nico.krijnen@luminis.eu>
2022-05-05 17:13:46 +0200

    Introduce layered architecture validation

---
+++ part-01/src/test/kotlin/eu/luminis/workshop/smallsteps/ArchitectureTest.kt
@@ -0,0 +1,25 @@
+package eu.luminis.workshop.smallsteps
+
+import com.tngtech.archunit.base.DescribedPredicate.alwaysTrue
+import com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.library.Architectures.*
+
+@AnalyzeClasses(packages = ["eu.luminis.workshop.smallsteps"])
+object ArchitectureTest {
+
+    @ArchTest
+    val layerDependenciesAreRespected: LayeredArchitecture = layeredArchitecture()
+        .layer("api").definedBy("eu.luminis.workshop.smallsteps.api..")
+        .layer("logic").definedBy("eu.luminis.workshop.smallsteps.logic..")
+        .layer("persistence").definedBy("eu.luminis.workshop.smallsteps.persistence..")
+        .whereLayer("api").mayNotBeAccessedByAnyLayer()
+        .whereLayer("logic").mayOnlyBeAccessedByLayers("api")
+        .whereLayer("persistence").mayOnlyBeAccessedByLayers("logic")
+        .ignoreDependency(
+            resideInAPackage("eu.luminis.workshop.smallsteps"),
+            alwaysTrue()
+        )
+
+}
