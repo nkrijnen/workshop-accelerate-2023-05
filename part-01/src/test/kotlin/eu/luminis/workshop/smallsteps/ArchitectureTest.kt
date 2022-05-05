a548510076445f8c4bfe576d01e352c098899194
Nico Krijnen <nico.krijnen@luminis.eu>
2022-05-05 17:17:20 +0200

    Introduce onion architecture validation

--- part-01/src/test/kotlin/eu/luminis/workshop/smallsteps/ArchitectureTest.kt
+++ part-01/src/test/kotlin/eu/luminis/workshop/smallsteps/ArchitectureTest.kt
@@ -9,14 +9,25 @@ import com.tngtech.archunit.library.Architectures.*
 @AnalyzeClasses(packages = ["eu.luminis.workshop.smallsteps"])
 object ArchitectureTest {
 
+//    @ArchTest
+//    val layerDependenciesAreRespected: LayeredArchitecture = layeredArchitecture()
+//        .layer("api").definedBy("eu.luminis.workshop.smallsteps.api..")
+//        .layer("logic").definedBy("eu.luminis.workshop.smallsteps.logic..")
+//        .layer("persistence").definedBy("eu.luminis.workshop.smallsteps.persistence..")
+//        .whereLayer("api").mayNotBeAccessedByAnyLayer()
+//        .whereLayer("logic").mayOnlyBeAccessedByLayers("api")
+//        .whereLayer("persistence").mayOnlyBeAccessedByLayers("logic")
+//        .ignoreDependency(
+//            resideInAPackage("eu.luminis.workshop.smallsteps"),
+//            alwaysTrue()
+//        )
+
     @ArchTest
-    val layerDependenciesAreRespected: LayeredArchitecture = layeredArchitecture()
-        .layer("api").definedBy("eu.luminis.workshop.smallsteps.api..")
-        .layer("logic").definedBy("eu.luminis.workshop.smallsteps.logic..")
-        .layer("persistence").definedBy("eu.luminis.workshop.smallsteps.persistence..")
-        .whereLayer("api").mayNotBeAccessedByAnyLayer()
-        .whereLayer("logic").mayOnlyBeAccessedByLayers("api")
-        .whereLayer("persistence").mayOnlyBeAccessedByLayers("logic")
+    val onionArchitectureIsRespected: OnionArchitecture? = onionArchitecture()
+        .domainModels("..logic..")
+        .withOptionalLayers(true)
+        .adapter("api", "..api..")
+        .adapter("persistence", "..persistence..")
         .ignoreDependency(
             resideInAPackage("eu.luminis.workshop.smallsteps"),
             alwaysTrue()
