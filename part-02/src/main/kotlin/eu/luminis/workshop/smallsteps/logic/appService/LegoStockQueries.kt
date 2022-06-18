3f20a3699e52897378b1db72adc1f5c1c5099978
Nico Krijnen <nico.krijnen@luminis.eu>
2022-06-18 22:05:40 +0200

    Use LegoParts in queries

--- part-02/src/main/kotlin/eu/luminis/workshop/smallsteps/logic/appService/LegoStockQueries.kt
+++ part-02/src/main/kotlin/eu/luminis/workshop/smallsteps/logic/appService/LegoStockQueries.kt
@@ -1,12 +1,14 @@
 package eu.luminis.workshop.smallsteps.logic.appService
 
+import eu.luminis.workshop.smallsteps.logic.domainModel.LegoParts
+import eu.luminis.workshop.smallsteps.logic.domainModel.toLegoParts
 import eu.luminis.workshop.smallsteps.logic.domainModel.valueObjects.LegoStoreId
 
 class LegoStockQueries(private val repository: LegoStockRepository) {
     fun currentlyMissingPartsReport(store: LegoStoreId): List<Pair<String, Int>> {
         val stockState = repository.find(store)
         return stockState.incompleteStock
-            .map { it.missingParts }
+            .map { it.missingParts.toLegoParts() }
             .mergeAndSum()
             .toSortedListOfPairs()
     }
@@ -14,22 +16,16 @@ class LegoStockQueries(private val repository: LegoStockRepository) {
     fun historicallyMostLostParts(store: LegoStoreId): List<Pair<String, Int>> {
         val stockState = repository.find(store)
         return stockState.incompleteReturnHistory
-            .map { it.missingParts }
+            .map { it.missingParts.toLegoParts() }
             .mergeAndSum()
             .toSortedListOfPairs()
     }
 }
 
-private fun List<Map<String, Int>>.mergeAndSum(): Map<String, Int> {
-    return this.fold(mapOf(), ::mergePartsAndSumValues)
+private fun List<LegoParts>.mergeAndSum(): LegoParts {
+    return this.fold(LegoParts(), LegoParts::plus)
 }
 
-fun mergePartsAndSumValues(parts: Map<String, Int>, otherParts: Map<String, Int>): Map<String, Int> {
-    val allPartCounts = parts.asSequence() + otherParts.asSequence()
-    val groupedByPartNr: Map<String, List<Int>> = allPartCounts.groupBy({ it.key }, { it.value })
-    return groupedByPartNr.mapValues { (_, values) -> values.sum() }
-}
-
-private fun Map<String, Int>.toSortedListOfPairs(): List<Pair<String, Int>> {
-    return this.toList().sortedBy { it.first }.sortedByDescending { it.second }
+private fun LegoParts.toSortedListOfPairs(): List<Pair<String, Int>> {
+    return this.toListOfPartCounts().sortedBy { it.first }.sortedByDescending { it.second }
 }
